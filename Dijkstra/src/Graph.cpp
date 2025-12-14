682cb7721631e8ee11a11f068da981165fab0953
Parsa <parsaileslamlou@gmail.com>
2025-12-14 15:32:55 -0800

    Full Graph.h and Graph.cpp

--- Dijkstra/src/Graph.cpp
+++ Dijkstra/src/Graph.cpp
@@ -9,15 +9,48 @@ Graph::Graph(const std::string& filename){
     parseFile(filename);
 }
 
-std::vector<std::pair<std::string, double>> getNeighbors(const std::string& node) const{
+//std::unordered_map<std::string, std::vector<std::pair<std::string, double>>> adjList;
+// Key is the starting Node, and value is an array of Nodes to visit with the weight
+std::vector<std::pair<std::string, double>> Graph::getNeighbors(const std::string& node) const{
     auto it = adjList.find(node);
     if (it != adjList.end()) return it->second;
-    return {}; // return empty vector if not found
+    return {};
 }
 
-void parseFile(const std::string& filename){
-    ifstream inFile;
-    inFile.open("data/campus_graph.txt");    
+void Graph::parseFile(const std::string& filename){
+    std::ifstream inFile;
+    inFile.open("data/campus_graph.txt");
+    
+    std::string str;
+    std::string key;
+    std::string destination;
+    double weight = 0.0;
+    int index = 1;
+    while(inFile >> str){
+        if(index == 5) index = 1;
+        if(index == 1) key = str;
+        else if(index == 2) destination = str;
+        else if(index == 3) weight = std::stod(str);
+        
+        else if(index == 4){
+            bool found = false;
+            if(str != "FLAT"){
+                weight *= (str == "STAIRS_UP" ? 1.8 : 1.2);
+            }
+            weight = std::round(weight * 1000.0) / 1000.0; // limits to 3 decimal places to ensure no floating point errors
+            
+            //Note for futre self: worst case is O(n) which can be a problem for a bigger datasets
+            //      if so, switch to an unordered_set for O(1)
+            for(auto it = adjList[key].begin(); it != adjList[key].end(); it++){
+                if(it->first == destination && it->second == weight){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) adjList[key].push_back({destination, weight});
+        }
+        index++;
+    }
 }
 
 
